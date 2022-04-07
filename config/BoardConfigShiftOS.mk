02b5af087275ad389d025758209db5a95b5f0cc1
Alexander Martinz <amartinz@shiftphones.com>
2022-04-07 21:03:47 +0200

    config: BoardConfig: remove GMS handling
    We are switching from out custom tailored version to
    what the LineageOS build system uses.
    
    Defaults are set when sourcing build/envsetup.sh and can
    be overriden with extra commands.
    
    Change-Id: I4970f5daf8e1fa069c3e5184c67cd6ec5ac22670
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- config/BoardConfigShiftOS.mk
+++ config/BoardConfigShiftOS.mk
@@ -37,32 +37,3 @@ TARGET_FACTORY_BUILD := false
 #
 # Factory
 #####
-
-#####
-# GMS
-#
-
-# Set GMS Home
-ANDROID_PARTNER_GMS_HOME := vendor/partner_gms
-
-# if WITH_GMS is set, use its value
-ifneq ($(WITH_GMS),)
-  SHIFT_BUILD_WITH_GMS := $(WITH_GMS)
-endif
-
-# if nothing is set for SHIFT_BUILD_WITH_GMS,
-# enable it by default
-ifeq ($(SHIFT_BUILD_WITH_GMS),)
-  SHIFT_BUILD_WITH_GMS := true
-endif
-
-ifneq ($(SHIFT_BUILD_WITH_GMS),)
-  BUILD_GMS := $(SHIFT_BUILD_WITH_GMS)
-endif
-
-# GMS Extras
-SHIFT_BUILD_WITH_GMS_EXTRA := false
-
-#
-# GMS
-#####
