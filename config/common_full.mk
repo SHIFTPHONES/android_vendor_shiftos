e58c41cd7ae6ec287400977e3a51b4bd3e5d5bc5
Alexander Martinz <amartinz@shiftphones.com>
2022-04-07 20:44:18 +0200

    config: inherit LineageOS configuration
    Change-Id: Ia917dabc9cf40dd5e51aac95840d2a6d11871107
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,1 +1,5 @@
+# Inherit from Lineage vendor
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from ShiftOS common
 $(call inherit-product, vendor/shiftos/config/common.mk)
