83892273c8fad4388562608ee1adb78b96b56a0d
Alexander Martinz <amartinz@shiftphones.com>
2024-04-02 17:23:34 +0200

    prebuilt: import bootanimations from sos-5.x
    Change-Id: I4c676c5536aa8b2dd119ddd66bf285f05626ae40
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

---
+++ config/bootanimation.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 SHIFT GmbH
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq ($(TARGET_SCREEN_WIDTH),)
+    $(warning TARGET_SCREEN_WIDTH is not set, using default value: 1080)
+    TARGET_SCREEN_WIDTH := 1080
+endif
+ifeq ($(TARGET_SCREEN_HEIGHT),)
+    $(warning TARGET_SCREEN_HEIGHT is not set, using default value: 1920)
+    TARGET_SCREEN_HEIGHT := 1920
+endif
+
+# e.g.: 1080x1920
+TARGET_SCREEN_RESOLUTION := $(TARGET_SCREEN_WIDTH)x$(TARGET_SCREEN_HEIGHT)
+
+# Boot and Shutdown animations
+PRODUCT_BOOTANIMATION := $(wildcard vendor/shiftos/prebuilt/$(TARGET_SCREEN_RESOLUTION)/media/bootanimation.zip)
+PRODUCT_SHUTANIMATION := $(wildcard vendor/shiftos/prebuilt/$(TARGET_SCREEN_RESOLUTION)/media/shutdownanimation.zip)
+
+ifneq ($(PRODUCT_BOOTANIMATION),)
+  TARGET_BOOTANIMATION := $(PRODUCT_BOOTANIMATION)
+endif
+
+ifneq ($(PRODUCT_SHUTANIMATION),)
+  TARGET_SHUTANIMATION := $(PRODUCT_SHUTANIMATION)
+endif
--- config/common.mk
+++ config/common.mk
@@ -10,7 +10,7 @@ PRODUCT_BRAND ?= SHIFT
 SHIFT_BUILD_WITH_GMS ?= true
 
 # Boot and Shutdown animations
-#include vendor/shiftos/config/bootanimation.mk
+include vendor/shiftos/config/bootanimation.mk
 
 # Packages
 #include vendor/shiftos/config/packages.mk
