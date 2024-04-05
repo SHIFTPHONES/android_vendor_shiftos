a4bc5c2de0ae056eabeba9707141081c57cdf07f
Bharath <bharath@teamb58.org>
2024-04-05 15:47:47 +0530

    Initial bringup of GMS components
    * Add a makefile call to include all GMS components(with EEA v2 support).
    * Add GMS client ID.
    * Add a makefile call to include all Mainline(GPSU) components.
    
    Issue: otter-19
    Change-Id: I5c5c2601a870da6f7fa54904cc813c0bbc998c8a

--- device-vendor.mk
+++ device-vendor.mk
@@ -4,4 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# GMS
+ifeq ($(SHIFT_BUILD_WITH_GMS),true)
+    # Set Google client ID as required for the approval process
+    PRODUCT_SYSTEM_PROPERTIES += \
+        ro.com.google.clientidbase=android-uniscope
 
+    # GMS components
+    $(call inherit-product, vendor/partner_gms/products/gms_eea_v2_type4c.mk)
+
+    # Add Mainline packages (GPSU).
+    $(call inherit-product, vendor/partner_modules/build/mainline_modules.mk)
+endif
