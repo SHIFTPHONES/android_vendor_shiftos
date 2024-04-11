9f26dc3cd5ea54b07027afd3f1b66d237faff362
Alexander Martinz <amartinz@shiftphones.com>
2024-04-11 14:11:46 +0200

    config: bootanimation: copy to product/media
    Change-Id: Ia9418cea27f7f85954606ff540f53a5615b3a092
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -21,9 +21,11 @@ PRODUCT_BOOTANIMATION := $(wildcard vendor/shiftos/prebuilt/$(TARGET_SCREEN_RESO
 PRODUCT_SHUTANIMATION := $(wildcard vendor/shiftos/prebuilt/$(TARGET_SCREEN_RESOLUTION)/media/shutdownanimation.zip)
 
 ifneq ($(PRODUCT_BOOTANIMATION),)
-  TARGET_BOOTANIMATION := $(PRODUCT_BOOTANIMATION)
+    PRODUCT_COPY_FILES += \
+        $(PRODUCT_BOOTANIMATION):$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 endif
 
 ifneq ($(PRODUCT_SHUTANIMATION),)
-  TARGET_SHUTANIMATION := $(PRODUCT_SHUTANIMATION)
+    PRODUCT_COPY_FILES += \
+        $(PRODUCT_SHUTANIMATION):$(TARGET_COPY_OUT_PRODUCT)/media/shutdownanimation.zip
 endif
