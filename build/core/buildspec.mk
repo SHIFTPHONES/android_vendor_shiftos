86fa5234f8ead166438f8e078a1ff651b9961206
Alexander Martinz <amartinz@shiftphones.com>
2024-08-16 11:00:51 +0200

    Revert "buildspec: force build date to 20240809"
    This reverts commit 60d80a6e33c58e6c59810dbda040f92a71ff0763.
    
    Change-Id: Ia12f54a76b539a4e9f3135c3da2499cb78a33efd
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- build/core/buildspec.mk
+++ build/core/buildspec.mk
@@ -1,9 +1,6 @@
 # configure build specifications here
 
-# Force build date and number to a fixed value
-#BUILD_NUMBER := $(shell date +%Y%m%d)
-BUILD_NUMBER := 20240809
-SHIFT_BUILD_DATE := 20240809
+BUILD_NUMBER := $(shell date +%Y%m%d)
 
 # Specify build user and host
 BUILD_USERNAME := shiftos
