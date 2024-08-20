ac13e446330989e5652ee47ce7289ac7110898d2
Alexander Martinz <amartinz@shiftphones.com>
2024-08-20 20:19:58 +0200

    config: packages: build SHIFT-Home
    Change-Id: I86b4eb19a25f36ca73169dbdde75f94d551bc467
    Issue: otter-48
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- config/packages.mk
+++ config/packages.mk
@@ -13,6 +13,10 @@ PRODUCT_PACKAGES += \
 -include vendor/shiftos/prebuilt/apps/F-Droid/F-Droid.mk
 PRODUCT_PACKAGES += additional_repos.xml-fdroid
 
+# Launcher
+PRODUCT_PACKAGES += \
+    SHIFT-Home
+
 # Music (Eleven)
 PRODUCT_PACKAGES += \
     Eleven
