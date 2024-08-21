aa5e0e178b0b4d18dca808507b02a30f8d5c89aa
Alexander Martinz <amartinz@shiftphones.com>
2024-08-21 18:35:14 +0200

    config: packages: build customization packages
    Issue: otter-39
    Change-Id: I253e3638719415edee3e8c4ecf8d275286feea5e
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- config/packages.mk
+++ config/packages.mk
@@ -9,6 +9,49 @@ PRODUCT_PACKAGES += \
     shiftos_charger_animation \
     shiftos_charger_animation_vendor \
 
+# Customizations - Fonts
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+
+PRODUCT_PACKAGES += \
+    fonts_customization.xml \
+    FontLatoOverlay \
+    FontRubikOverlay
+
+# Customizations - Icons
+PRODUCT_PACKAGES += \
+    IconPackCircularAndroidOverlay \
+    IconPackCircularLauncherOverlay \
+    IconPackCircularSettingsOverlay \
+    IconPackCircularSystemUIOverlay \
+    IconPackFilledAndroidOverlay \
+    IconPackFilledLauncherOverlay \
+    IconPackFilledSettingsOverlay \
+    IconPackFilledSystemUIOverlay \
+    IconPackKaiAndroidOverlay \
+    IconPackKaiLauncherOverlay \
+    IconPackKaiSettingsOverlay \
+    IconPackKaiSystemUIOverlay \
+    IconPackRoundedAndroidOverlay \
+    IconPackRoundedLauncherOverlay \
+    IconPackRoundedSettingsOverlay \
+    IconPackRoundedSystemUIOverlay \
+    IconPackSamAndroidOverlay \
+    IconPackSamLauncherOverlay \
+    IconPackSamSettingsOverlay \
+    IconPackSamSystemUIOverlay \
+    IconPackVictorAndroidOverlay \
+    IconPackVictorLauncherOverlay \
+    IconPackVictorSettingsOverlay \
+    IconPackVictorSystemUIOverlay \
+    IconShapePebbleOverlay \
+    IconShapeRoundedRectOverlay \
+    IconShapeSquareOverlay \
+    IconShapeSquircleOverlay \
+    IconShapeTaperedRectOverlay \
+    IconShapeTeardropOverlay \
+    IconShapeVesselOverlay
+
 # F-Droid
 -include vendor/shiftos/prebuilt/apps/F-Droid/F-Droid.mk
 PRODUCT_PACKAGES += additional_repos.xml-fdroid
