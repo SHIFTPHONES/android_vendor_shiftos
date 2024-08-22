9ad9af9fe7b8720458598489a7ec1a55b1c8dea3
Alexander Martinz <amartinz@shiftphones.com>
2024-08-22 19:42:30 +0200

    Revert "config: common: do not include private signing config"
    This reverts commit a6a715b302863d3caf110e5c5777b96ed197eb5b.
    
    Change-Id: I78613cd1026aff3633c2f45d105bbda449977cd2
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- config/common.mk
+++ config/common.mk
@@ -26,5 +26,8 @@ include vendor/shift/apps/esim.mk
 # ShiftOS permissions
 include vendor/shiftos/config/permissions.mk
 
+# Private bits if existing
+-include vendor/shiftos/private/common.mk
+
 # Include ADB keys for debuggable builds
 -include vendor/shiftos/security/adb/adb_keys.mk
