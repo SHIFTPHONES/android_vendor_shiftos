0f5f65d2a766f8b8f9163431491eeb38a5ee827f
Alexander Martinz <amartinz@shiftphones.com>
2024-12-05 13:20:01 +0100

    config: common_full: inherit from lineage
    Change-Id: I32caf250294c2f923bb87e6c579346feb787660e
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- config/common_full.mk
+++ config/common_full.mk
@@ -8,5 +8,8 @@
 # before including any other vendor overlays to allow overriding them.
 $(call inherit-product, vendor/shiftos/config/common_overlays.mk)
 
+# Inherit from Lineage vendor
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 # Inherit from ShiftOS common
 $(call inherit-product, vendor/shiftos/config/common.mk)
