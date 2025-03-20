27bbf9b72b8316a2bf6d937d00268bf61ffffd5b
Alexander Martinz <amartinz@shiftphones.com>
2025-03-20 20:16:38 +0100

    versioning: move to PRODUCT_SYSTEM_PROPERTIES
    ADDITIONAL_SYSTEM_PROPERTIES is/will be gone.
    
    Change-Id: Ica03811bd6ee90be3d3973b1a810c089b393a5f4
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- config/version.mk
+++ config/version.mk
@@ -34,3 +34,16 @@ else
 endif
 
 ####################################################################################
+
+# ShiftOS system version
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.shift.sos.version.extra="$(SHIFT_SOS_VERSION_EXTRA)" \
+    ro.shift.sos.version.name="$(SHIFT_SOS_VERSION_NAME)" \
+    ro.shift.sos.version.number="$(SHIFT_SOS_VERSION_NUMBER)" \
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.shift.version.display="$(SHIFT_VERSION_DISPLAY)" \
+
+# ShiftOS release type
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.shift.release.type="$(SHIFT_SOFTWARE_BUILD_TYPE)" \
