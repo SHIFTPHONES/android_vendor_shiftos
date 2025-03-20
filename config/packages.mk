144484b87fc68b36e011110267174ac4f961edb6
Alexander Martinz <amartinz@shiftphones.com>
2025-03-20 17:09:21 +0100

    config: packages: prepare for G vs L
    Change-Id: I15a803291a2185704610f8ed63da800c1363c430
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- config/packages.mk
+++ config/packages.mk
@@ -93,3 +93,13 @@ PRODUCT_PACKAGES += \
 
 DEVICE_PACKAGE_OVERLAYS += \
     vendor/shiftos/overlay
+
+ifeq ($(WITH_GMS),true)
+
+  # ShiftOS-G specific packages
+
+else
+
+  # ShiftOS-L specific packages
+
+endif
