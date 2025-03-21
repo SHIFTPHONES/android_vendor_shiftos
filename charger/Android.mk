93a1d8338b168f3c4f2765d98c2a937918720834
Alexander Martinz <amartinz@shiftphones.com>
2025-03-21 17:47:47 +0100

    charger: import from sos-6.x
    Change-Id: I2b3c3919601fd1a98df3ed12873b7006d937a94d
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- charger/Android.mk
+++ charger/Android.mk
@@ -7,10 +7,24 @@
 
 LOCAL_PATH := $(call my-dir)
 
+## Set shiftos_charger_density to the density bucket of the device.
+#shiftos_charger_density := mdpi
+#ifneq (,$(TARGET_SCREEN_DENSITY))
+#shiftos_charger_density := $(strip \
+#  $(or $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 560))),1),xxxhdpi),\
+#       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 400))),1),xxhdpi),\
+#       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 280))),1),xhdpi),\
+#       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 200))),1),hdpi,mdpi)))
+#else ifneq (,$(filter mdpi hdpi xhdpi xxhdpi xxxhdpi,$(PRODUCT_AAPT_PREF_CONFIG)))
+## If PRODUCT_AAPT_PREF_CONFIG includes a dpi bucket, then use that value.
+#shiftos_charger_density := $(PRODUCT_AAPT_PREF_CONFIG)
+#endif
+shiftos_charger_density := shift
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := shiftos_charger_battery_scale
 LOCAL_MODULE_STEM := battery_scale.png
-LOCAL_SRC_FILES := images/battery_scale.png
+LOCAL_SRC_FILES := $(shiftos_charger_density)/battery_scale.png
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/res/images/charger
@@ -19,7 +33,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := shiftos_charger_battery_scale_vendor
 LOCAL_MODULE_STEM := battery_scale.png
-LOCAL_SRC_FILES := images/battery_scale.png
+LOCAL_SRC_FILES := $(shiftos_charger_density)/battery_scale.png
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/res/images/charger
@@ -28,7 +42,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := shiftos_charger_battery_fail
 LOCAL_MODULE_STEM := battery_fail.png
-LOCAL_SRC_FILES := images/battery_fail.png
+LOCAL_SRC_FILES := $(shiftos_charger_density)/battery_fail.png
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/res/images/charger
@@ -37,25 +51,25 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := shiftos_charger_battery_fail_vendor
 LOCAL_MODULE_STEM := battery_fail.png
-LOCAL_SRC_FILES := images/battery_fail.png
+LOCAL_SRC_FILES := $(shiftos_charger_density)/battery_fail.png
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/res/images/charger
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := shiftos_charger_main_font
-LOCAL_MODULE_STEM := main_font.png
-LOCAL_SRC_FILES := images/main_font.png
+LOCAL_MODULE := shiftos_charger_font
+LOCAL_MODULE_STEM := percent_font.png
+LOCAL_SRC_FILES := $(shiftos_charger_density)/percent_font.png
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/res/images/charger
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := shiftos_charger_main_font_vendor
-LOCAL_MODULE_STEM := main_font.png
-LOCAL_SRC_FILES := images/main_font.png
+LOCAL_MODULE := shiftos_charger_font_vendor
+LOCAL_MODULE_STEM := percent_font.png
+LOCAL_SRC_FILES := $(shiftos_charger_density)/percent_font.png
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/res/images/charger
@@ -68,7 +82,7 @@ LOCAL_SRC_FILES := animation.txt
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/res/values/charger
-LOCAL_REQUIRED_MODULES := shiftos_charger_battery_scale shiftos_charger_battery_fail shiftos_charger_main_font
+LOCAL_REQUIRED_MODULES := shiftos_charger_battery_scale shiftos_charger_battery_fail shiftos_charger_font
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -78,5 +92,5 @@ LOCAL_SRC_FILES := animation.txt
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/res/values/charger
-LOCAL_REQUIRED_MODULES := shiftos_charger_battery_scale_vendor shiftos_charger_battery_fail_vendor shiftos_charger_main_font_vendor
+LOCAL_REQUIRED_MODULES := shiftos_charger_battery_scale_vendor shiftos_charger_battery_fail_vendor shiftos_charger_font_vendor
 include $(BUILD_PREBUILT)
