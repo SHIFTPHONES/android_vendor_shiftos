59896d8acabfca18f28ce8c4739c9cb7bc3d7ef9
Alexander Martinz <amartinz@shiftphones.com>
2025-03-28 16:34:58 +0100

    config: BoardConfigShiftOS: include sepolicy from device/shift/sepolicy
    Change-Id: Ib874150a4fc933985aeb16d39f454efabd108644
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- config/BoardConfigShiftOS.mk
+++ config/BoardConfigShiftOS.mk
@@ -1,7 +1,16 @@
 #
-# Copyright (C) 2024 SHIFT GmbH
+# Copyright (C) SHIFT GmbH
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # BoardConfig common to all targets belong here
+
+####
+# Sepolicy
+#
+
+include device/shift/sepolicy/common/sepolicy.mk
+
+#
+####
