f1a177cdda6c34ee12895a394a354c74d771d087
Alexander Martinz <amartinz@shiftphones.com>
2025-04-02 20:02:46 +0200

    (WIP) build: target: sdk: configure for sdk_addon target
    - m sdk_addon
    
    Change-Id: I849f271a4fb6daf05fcce1ebbcdcc9693a233f0a
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- build/target/product/shiftos_generic_target.mk
+++ build/target/product/shiftos_generic_target.mk
@@ -9,3 +9,11 @@ $(call inherit-product, vendor/shiftos/config/common_full.mk)
 
 # Allow building otatools
 TARGET_FORCE_OTA_PACKAGE := true
+
+# Rules for public APIs
+PRODUCT_SDK_ADDON_STUB_DEFS := vendor/shiftos/build/target/product/sdk_addon_stub_defs.txt
+
+# Keyhandler
+PRODUCT_PACKAGES += eco.shift.os.keyhandler
+PRODUCT_SDK_ADDON_COPY_MODULES += eco.shift.os.keyhandler:libs/eco.shift.os.keyhandler.jar
+#PRODUCT_SDK_ADDON_DOC_MODULES += eco.shift.os.keyhandler-doc
