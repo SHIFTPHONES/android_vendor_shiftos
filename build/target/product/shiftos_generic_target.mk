ea0f709123162004a655ab64454dccb08288938b
Alexander Martinz <amartinz@shiftphones.com>
2025-04-02 20:02:46 +0200

    build: target: sdk: configure for sdk_addon target
    - lunch shiftos_sdk_phone_x86_64-ap4a-userdebug
    - m sdk_addon
    
    Change-Id: I849f271a4fb6daf05fcce1ebbcdcc9693a233f0a
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- build/target/product/shiftos_generic_target.mk
+++ build/target/product/shiftos_generic_target.mk
@@ -9,3 +9,24 @@ $(call inherit-product, vendor/shiftos/config/common_full.mk)
 
 # Allow building otatools
 TARGET_FORCE_OTA_PACKAGE := true
+
+PRODUCT_SDK_ADDON_NAME := ShiftOS
+PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := vendor/shiftos/build/target/product/source.properties
+
+# Copy the manifest and properties files for the SDK add-on.
+PRODUCT_SDK_ADDON_COPY_FILES += \
+    vendor/shiftos/build/target/product/manifest.ini:manifest.ini \
+    vendor/shiftos/build/target/product/package.xml:package.xml \
+
+# Rules for public APIs
+PRODUCT_SDK_ADDON_STUB_DEFS += vendor/shiftos/build/target/product/sdk_addon_stub_defs.txt
+
+# SDK
+PRODUCT_PACKAGES += eco.shift.sdk
+PRODUCT_SDK_ADDON_COPY_MODULES += eco.shift.sdk:libs/eco.shift.sdk.jar
+
+ifeq ($(HOST_ARCH),x86_64)
+    INTERNAL_SDK_HOST_OS_NAME := linux-x86
+else
+    INTERNAL_SDK_HOST_OS_NAME := linux-$(REAL_HOST_ARCH)
+endif
