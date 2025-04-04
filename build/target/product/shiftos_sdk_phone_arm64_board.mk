bf0b6b1c3220f19aa6857915fdd6b2424dd857ff
Alexander Martinz <amartinz@shiftphones.com>
2025-04-04 11:56:29 +0200

    fixup! build: target: add sdk phone target
    Change-Id: I63a27a64133b2841046ae895f70d0674c88c1bce
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- build/target/product/shiftos_sdk_phone_arm64_board.mk
+++ build/target/product/shiftos_sdk_phone_arm64_board.mk
@@ -11,6 +11,6 @@ BOARD_EMULATOR_DYNAMIC_PARTITIONS_SIZE := 10737418240
 
 # Packaging sdk_addon target
 PRODUCT_SDK_ADDON_COPY_FILES += \
-    device/generic/goldfish/data/etc/advancedFeatures.ini.arm:images/arm64-v8a/advancedFeatures.ini \
+    device/generic/goldfish/data/etc/advancedFeatures.ini:images/arm64-v8a/advancedFeatures.ini \
     device/generic/goldfish/data/etc/encryptionkey.img:images/arm64-v8a/encryptionkey.img \
     $(EMULATOR_KERNEL_FILE):images/arm64-v8a/kernel-ranchu
